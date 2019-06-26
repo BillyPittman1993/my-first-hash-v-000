8aa6bb8fc348e295a567980a794f040c89488aa8
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-26 15:43:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,13 +1,9 @@
 def my_hash
- your_hash = {"name" => "Billy", "profession" => "Advisor"}
+ 
 end
 
 def shipping_manifest
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                  }
+ 
 end
 
 def retrieval
